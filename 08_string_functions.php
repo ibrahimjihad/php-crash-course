109cfde23e00917c118599e3e89f7bea1f4b7fa2
ibrahimjihad <ibrahimjihad2000@gmail.com>
2025-04-22 03:04:00 +0600

    added super globals and modified string_functions

--- 08_string_functions.php
+++ 08_string_functions.php
@@ -10,19 +10,19 @@ $string = 'Hello World';
 
 // Get the length of a string
 echo strlen($string);
-
+ECHO "<br>";
 // Find the position of the first occurrence of a substring in a string
 echo strpos($string, 'o');
-
+ECHO "<br>";
 // Find the position of the last occurrence of a substring in a string
 echo strrpos($string, 'o');
-
+ECHO "<br>";
 // Reverse a string
 echo strrev($string);
-
+ECHO "<br>";
 // Convert all characters to lowercase
 echo strtolower($string);
-
+ECHO "<br>";
 // Convert all characters to uppercase
 echo strtoupper($string);
 
